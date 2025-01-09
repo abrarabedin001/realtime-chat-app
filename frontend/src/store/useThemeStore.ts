37f83ede8cc54b506e64b3d4087384ab11581957
abrarabedin001 <abrarabedin001@gmail.com>
2025-01-10 01:37:47 +0600

    fixed chatlayout

---
+++ frontend/src/store/useThemeStore.ts
@@ -0,0 +1,29 @@
+import { create } from "zustand"
+
+
+
+
+import { devtools, persist } from 'zustand/middleware';
+
+interface useThemeStoreState {
+  theme: string;
+  setTheme: (theme: string) => void;
+
+
+
+}
+
+export const useThemeStore = create<useThemeStoreState>()(
+  devtools(
+    persist(
+      (set) => ({
+        theme: localStorage.getItem("chat-theme") || "coffee",
+        setTheme: (theme) => {
+          localStorage.setItem("chat-theme", theme);
+          set({ theme });
+        },
+      }),
+      { name: 'useThemeStore' },
+    ),
+  )
+)
--- frontend/store/useAuthStore.ts
+++ frontend/store/useAuthStore.ts
@@ -1,4 +1,4 @@
-import { axiosInstance } from './../lib/axios';
+import { axiosInstance } from '../src/lib/axios';
 import { create } from "zustand"
 
 import { io, Socket } from "socket.io-client";
